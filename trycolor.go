2d91ad12446c9239abed6a5cbd26aa41e82e1e80
Dmitriy Kopylenko <dima767@gmail.com>
2017-03-01 16:26:40 -0500

    Initial commit

---
+++ trycolor.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/fatih/color"
+
+func main() {
+	color.Cyan("Prints text in cyan.")
+
+	// a newline will be appended automatically
+	color.Blue("Prints %s in blue.", "text")
+
+	// More default foreground colors..
+	color.Red("We have red")
+	color.White("WHITE!!!")
+	color.Yellow("Yellow color too!")
+	color.Magenta("And many others ..")
+
+	// Mix up foreground and background colors, create new mixes!
+	/*red := color.New(color.FgRed)
+	boldRed := red.Add(color.Bold)
+	boldRedPrintf := boldRed.FprintFunc()
+	boldRedPrintf("This will print text in bold red.")*/
+
+	// Create a custom print function for convenient
+	red := color.New(color.Bold, color.FgHiGreen).PrintfFunc()
+	red("warning...\n")
+
+}
